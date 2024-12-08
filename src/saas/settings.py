06fb767ef1a06df8ba74d3361a721552eb326aa9
amirali <ososgol@gmail.com>
2024-12-08 20:19:38 +0330

    create a docker and railway|v--0.3.2

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)f$mkgtp$j97k5uowsj73y104b!y69!2#o#ek^*o+@swdp6#9'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', cast=bool)
 
+print('DEBUG', DEBUG)
 ALLOWED_HOSTS = [
 ".railway.app"
 ]
