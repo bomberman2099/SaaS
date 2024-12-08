aced12de1d1bcf39d5093703261c0955d37fdcfb
amirali <ososgol@gmail.com>
2024-12-08 17:59:19 +0330

    create a about page|v--0.3.1

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -17,3 +17,15 @@ def home_page_view(request):
         'page_title': page_title,
         'visited_count': page_qs.count(),
         'total_visits': qs.count()})
+
+
+def about_page_view(request):
+    page_title = "About Page"
+    path = request.path
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=path)
+    PageVisit.objects.create(path=path)
+    return render(request, 'cfehome/about.html', {
+        'page_title': page_title,
+        'visited_count': page_qs.count(),
+        'total_visits': qs.count()})
