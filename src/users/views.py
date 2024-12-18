f50b539e22072bec6ed4d4812bc514693ab2fa70
amirali <ososgol@gmail.com>
2024-12-18 19:49:36 +0330

    add register view and form|v--0.6.2

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
-
+User = get_user_model()
 # Create your views here.
 
 def login_view(request):
@@ -14,3 +15,15 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
     return render(request, 'users/login.html', {})
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        # username_exists = User.objects.filter(username__iexact=username).exists()
+        # email_exists = User.objects.filter(email__iexact=email).exists()
+        User.objects.create(username=username, email=email)
+        return redirect('home')
+    return render(request, 'users/register.html', {})
