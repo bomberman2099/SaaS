a9fca2791dd5638e0d324cb2072069cc8081b0bb
amirali <ososgol@gmail.com>
2024-12-20 20:40:11 +0330

    add staff$user only tests view |v--0.6.6

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home_page_view, name='home'),
     path('about/', views.about_page_view, name='about'),
+    path('protected/', views.pw_protected_view, name='protected'),
+    path('protected/user_only/', views.user_only, name='user_only'),
+    path('protected/staff_only/', views.staff_only, name='staff_only'),
 
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from visits.models import PageVisit
-
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 # Create your views here.
 
-
+login_url = settings.LOGIN_URL
 def home_page_view(request):
     page_title = "Home Page"
     path = request.path
@@ -29,3 +30,28 @@ def about_page_view(request):
         'page_title': page_title,
         'visited_count': page_qs.count(),
         'total_visits': qs.count()})
+
+
+valid_code = 'abc'
+def pw_protected_view(request):
+    is_allowed = request.session.get('protected_page_allowed') or None
+    print(is_allowed, type(is_allowed))
+    if request.method == "POST":
+        user_pw_send = request.POST['code'] or None
+        if user_pw_send == valid_code:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+
+    if is_allowed:
+        return render(request, 'protected/view.html', {})
+    return render(request, 'protected/entry.html', {})
+
+
+@login_required(login_url=login_url)
+def user_only(request):
+    return render(request, 'protected/user_only.html', {})
+
+
+@staff_member_required(login_url=login_url)
+def staff_only(request):
+    return render(request, 'protected/user_only.html', {})
