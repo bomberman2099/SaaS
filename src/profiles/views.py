22b6b74a4b168d5a9817d603961206c1af652809
amirali <ososgol@gmail.com>
2024-12-22 23:16:02 +0330

    add profiles views list&details |v--0.6.7

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -17,7 +17,6 @@ def profile_details_view(request, username):
     user = request.user
     print(user.has_perm("auth.view_user"))
     print(user.has_perm("visits.view_pagevisit"))
-    # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     context = {
         "object": profile_user_obj,
