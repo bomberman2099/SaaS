153d17f73d41e9ba1aff9fc0de007a44ee9867c8
amirali <ososgol@gmail.com>
2024-12-25 20:11:30 +0330

    sync user subscription with the user group |v--0.6.9.2

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -6,6 +6,8 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+
+AlLOW_CUSTOM_GROUPS = True
 SUBSCRIPTION_PERMISSIONS = [
     ("advanced", "Advanced Perm"),  # subscriptions.advanced
     ("pro", "Pro Perm"),
@@ -41,8 +43,25 @@ def user_sub_post_save(sender, instance,*args, **kwargs):
     user_sub_instance = instance # UserSubscription obj
     user = user_sub_instance.user # obj.user
     subscription_obj = user_sub_instance.subscription
-    groups = subscription_obj.groups.all()
-    user.groups.set(groups)
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    if not AlLOW_CUSTOM_GROUPS :
+        user.groups.set(groups)
+    else:
+        subs_qs = (Subscription.objects.filter(active=True))
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=user_sub_instance.id)
+
+        subs_groups = subs_qs.values_list('groups__id', flat=True)
+        subs_groups_set = set(subs_groups)
+        currents_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids)
+        currents_groups_set = set(currents_groups) - subs_groups_set
+        final_group_ids = list(groups_ids_set | currents_groups_set)
+        user.groups.set(final_group_ids)
+
 
 
 post_save.connect(user_sub_post_save, sender=UserSubscription)
