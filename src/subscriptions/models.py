e1ac17e1599d641d298d7cf8de18009840fcb8b7
amirali <ososgol@gmail.com>
2024-12-25 17:47:28 +0330

    update subscriptions model |v--0.6.9

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import Group, Permission
 # Create your models here.
 
-class Subscription(models.Model):
-    name = models.CharField(max_length=120)
 
-    class Meta:
-        permissions = [
+
+SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advanced Perm"), # subscriptions.advanced
-            ("pro", "Pro Perm"), 
+            ("pro", "Pro Perm"),
             ("basic", "Basic Perm"),
             ("basic_ai", "Basic AI Perm"),
         ]
+
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", 'codename__in': [X[0] for X in SUBSCRIPTION_PERMISSIONS]})
+    class Meta:
+        permissions = SUBSCRIPTION_PERMISSIONS
