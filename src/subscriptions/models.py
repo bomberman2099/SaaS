33bc98a2db936fb9cf6b2673b441c5e8d1b81df9
amirali <ososgol@gmail.com>
2024-12-25 18:30:33 +0330

    syncing subscription groups and permissions |v--0.6.9.1

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -15,6 +15,10 @@ SUBSCRIPTION_PERMISSIONS = [
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
     groups = models.ManyToManyField(Group)
+    active = models.BooleanField(default=True)
     permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", 'codename__in': [X[0] for X in SUBSCRIPTION_PERMISSIONS]})
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+    def __str__(self):
+        return self.name
