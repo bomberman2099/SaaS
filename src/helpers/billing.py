f44ba8dcf0021a0e9fc1faa71b7dee3628debb4f
amirali <ososgol@gmail.com>
2024-12-29 19:29:11 +0330

    getting started with stripe config |v--0.7.0

---
+++ src/helpers/billing.py
@@ -0,0 +1,17 @@
+# Set your secret key. Remember to switch to your live secret key in production.
+# See your keys here: https://dashboard.stripe.com/apikeys
+import stripe
+from decouple import config
+
+DJANGO_DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', default="", cast=str)
+
+if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
+  raise ValueError("STRIPE_SECRET_KEY must be set in DEBUG mode")
+
+
+stripe.api_key = "sk_test_VePHdqKTYQjKNInc7u56JBrQ"
+# stripe.Customer.create(
+#   name="Jenny Rosen",
+#   email="jennyrosen@example.com",
+# )
