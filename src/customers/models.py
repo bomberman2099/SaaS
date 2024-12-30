8c11b4b4908f2a31c5b7c42885922c8ae3448c50
amirali <ososgol@gmail.com>
2024-12-30 22:01:32 +0330

    upgrade model save method for customers |v--0.7.1

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,4 @@
+import helpers.billing
 from django.db import models
 from django.conf import settings
 # Create your models here.
@@ -10,3 +11,14 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+    def save(self, *args, **kwargs):
+        if not self.stripe_id:
+
+            email = self.user.email
+            if email != "" or email is not None:
+                stripe_id = helpers.billing.create_customer(email=email, raw=True)
+                self.stripe_id = stripe_id
+
+        super().save(*args, **kwargs)
--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -11,7 +11,13 @@ if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
 
 
 stripe.api_key = "sk_test_VePHdqKTYQjKNInc7u56JBrQ"
-# stripe.Customer.create(
-#   name="Jenny Rosen",
-#   email="jennyrosen@example.com",
-# )
+def create_customer(name="", email="",  raw=False):
+
+  response = stripe.Customer.create(
+    name=name,
+    email=email,
+  )
+  if raw:
+    return response
+  stripe_id = response.id
+  return stripe_id
